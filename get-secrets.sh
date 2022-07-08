68b99bfb4a0abd0283d080f02ee48967c0843b9a
Jessica Deen <jessicadeen@me.com>
2022-07-08 16:36:13 -0700

    bump hugo version (#4)
    * bump hugo version
    
    Signed-off-by: jldeen <jessicadeen@me.com>

--- get-secrets.sh
+++ get-secrets.sh
@@ -7,14 +7,18 @@ set -e
 
 # Set the following
 spName=resume
-subName="ca-jessde-demo-test"
+subName="jdc"
 project="resume"
+location="eastus2"
 
 # set the subscription
 az account set --subscription "$subName" && echo "Your default subscription has been set to: $subName"
 
 # Get Subscription ID
 subscriptionId=$(az account show --query id -o tsv)
+
+# Create Resource group
+az group create --name $project --location $location
 
 # Create a service principal
     echo "Creating service principal..."
@@ -24,7 +28,11 @@ subscriptionId=$(az account show --query id -o tsv)
             --scopes /subscriptions/$subscriptionId/resourceGroups/$project)
 
     # save spInfo locally
-    echo $spInfo > auth.json        
+    echo $spInfo > auth.json
+
+    #ensure role assignment is created
+    clientId=$(echo $spInfo | jq -r '.clientId')
+    az role assignment create --role "Owner" --assignee $clientId && echo "Owner role assignment created for $clientId"       
 
     if [ $? == 0 ]; then
         
